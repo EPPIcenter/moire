8060dfdca3e1779cb1a3256f59d252e1bbfbd67e
Max Murphy <murphy2122@berkeley.edu>
2019-05-14 09:01:52 -0700

    update documentation

--- R/mcmc.R
+++ R/mcmc.R
@@ -1,4 +1,4 @@
-run_prof_mcmc_rcpp <- function(args) {
+.run_prof_mcmc_rcpp <- function(args) {
   .Call("start_profiler", "samples.log")
 
   res <- .Call(`_moiR_run_mcmc`, args)
@@ -8,7 +8,27 @@ run_prof_mcmc_rcpp <- function(args) {
   return(res)
 }
 
-run_mcmc <- function(data, thin = 0, burnin = 1e4, samples = 1e4, num_chains = 1, importance_sampling_depth = 20, max_coi = 30, eps_pos_0 = .01, eps_pos_var=.25, eps_pos_alpha = 2, eps_pos_beta = 100, eps_neg_0 = .05, eps_neg_var=.25, eps_neg_alpha = 6, eps_neg_beta = 96, max_eps_pos = .2, max_eps_neg = .2, alpha = 200) {
+#' Run the MCMC
+#' @export
+#'
+#' @param data Data to be used in MCMC
+#' @param thin How often to sample from mcmc, 0 means do not thin
+#' @param burnin Number of MCMC samples to discard as burnin
+#' @param samples Number of samples to take after burnin
+#' @param num_chains Number of concurrent chains to run (not implemented)
+#' @param importance_sampling_depth How many samples to take during importance sampling
+#' @param max_coi Maximum allowed complexity of infection
+#' @param eps_pos_0 Initial eps_pos value
+#' @param eps_pos_var Variance used in sampling eps_pos
+#' @param eps_pos_alpha Alpha parameter in Beta distribution for eps_pos prior
+#' @param eps_pos_beta Beta parameter in Beta distribution for eps_pos prior
+#' @param eps_neg_0 Initial eps_neg value
+#' @param eps_neg_var Variance used in sampling eps_neg
+#' @param eps_neg_alpha Alpha parameter in Beta distribution for eps_neg prior
+#' @param eps_neg_beta Beta parameter in Beta distribution for eps_neg prior
+#' @param max_eps_pos Maximum allowed value for eps_pos
+#' @param max_eps_neg Maximum allowed value for eps_neg
+run_mcmc <- function(data, thin = 0, burnin = 1e4, samples = 1e4, num_chains = 1, importance_sampling_depth = 20, max_coi = 30, eps_pos_0 = .01, eps_pos_var=.25, eps_pos_alpha = 2, eps_pos_beta = 100, eps_neg_0 = .05, eps_neg_var=.25, eps_neg_alpha = 6, eps_neg_beta = 96, max_eps_pos = .2, max_eps_neg = .2) {
   args <- as.list(environment())
   res <- run_mcmc_rcpp(args)
   res
--- R/simulator.R
+++ R/simulator.R
@@ -1,21 +1,28 @@
-sim_locus_allele_frequencies <- function(alpha, num_loci) {
-  dists <- MCMCpack::rdirichlet(num_loci, alpha)
+.rdirichlet <- function(n, alpha) {
+  len_alpha <- length(alpha)
+  d <- matrix(rgamma(len_alpha * n, alpha), ncol = len_alpha, byrow = TRUE)
+  m <- d %*% rep(1, len_alpha)
+  d / as.vector(m)
+}
+
+.sim_locus_allele_frequencies <- function(alpha, num_loci) {
+  dists <- rdirichlet(num_loci, alpha)
   lapply(seq_len(num_loci), function(x) {
     dists[x,]
   })
 }
 
-sim_sample_moi <- function(num_samples, mean_moi) {
+.sim_sample_moi <- function(num_samples, mean_moi) {
   qpois(runif(num_samples, dpois(0, mean_moi), 1), mean_moi)
 }
 
-sim_sample_genotype <- function(sample_mois, locus_allele_dist) {
+.sim_sample_genotype <- function(sample_mois, locus_allele_dist) {
   lapply(sample_mois, function(moi) {
     rmultinom(1, moi, locus_allele_dist)
   })
 }
 
-sim_observed_allele <- function(alleles, epsilon_pos, epsilon_neg) {
+.sim_observed_allele <- function(alleles, epsilon_pos, epsilon_neg) {
   sapply(alleles, function(allele) {
     if (allele) {
       rbinom(1, 1, prob = 1 - epsilon_pos ** allele)
@@ -25,27 +32,38 @@ sim_observed_allele <- function(alleles, epsilon_pos, epsilon_neg) {
   })
 }
 
-sim_observed_genotype <- function(true_genotypes, epsilon_pos, epsilon_neg) {
+.sim_observed_genotype <- function(true_genotypes, epsilon_pos, epsilon_neg) {
   lapply(true_genotypes, function(x) {
-    sim_observed_allele(x, epsilon_pos, epsilon_neg)
+    .sim_observed_allele(x, epsilon_pos, epsilon_neg)
   })
 }
 
+#' Simulate data that is compatible with run_mcmc
+#'
+#' @export
+#'
+#' @param mean_moi Mean multiplicity of infection drawn from a poisson
+#' @param num_loci Number of genetic loci to simulate
+#' @param locus_freq_alpha List of alphas to be used to simulate from a dirichlet distribution to generate allele frequencies. Generates num_loci distributions for each alpha
+#' @param num_samples Total number of biological samples to simulate
+#' @param epsilon_pos False positive rate
+#' @param epsilon_neg False negative rate
+#' @return Simulated data that is structured to go into the MCMC sampler
+#'
 sim_data <- function(mean_moi, num_loci, locus_freq_alpha, num_samples, epsilon_pos, epsilon_neg) {
-  # allele_freq_dists <- sim_locus_allele_frequencies(locus_freq_alpha, num_loci)
   allele_freq_dists <- c()
   for (alpha in locus_freq_alpha) {
-    allele_freq_dists <- c(allele_freq_dists, sim_locus_allele_frequencies(alpha, num_loci))
+    allele_freq_dists <- c(allele_freq_dists, .sim_locus_allele_frequencies(alpha, num_loci))
   }
 
-  sample_mois <- sim_sample_moi(num_samples, mean_moi)
+  sample_mois <- .sim_sample_moi(num_samples, mean_moi)
 
   true_sample_genotypes <- lapply(allele_freq_dists, function(dist) {
-    sim_sample_genotype(sample_mois, dist)
+    .sim_sample_genotype(sample_mois, dist)
   })
 
   observed_sample_genotypes <- lapply(true_sample_genotypes, function(locus_genotypes) {
-    sim_observed_genotype(locus_genotypes, epsilon_pos, epsilon_neg)
+    .sim_observed_genotype(locus_genotypes, epsilon_pos, epsilon_neg)
   })
 
   list(
